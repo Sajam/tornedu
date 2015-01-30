2329696e75080067085fe43b0ffa6c0bfdaa105e
Daniel Kowalczyk <d.sajam@gmail.com>
2015-01-30 22:04:12 +0100

    Basic layout/design & header

---
+++ static/js/general.js
@@ -0,0 +1,14 @@
+$(function () {
+    $(document).on('click', function (e) {
+        // If not clicked in user area then close user popover.
+        if (!$(e.target).closest('.user').length) {
+            $('.user').removeClass('popover-open');
+            $('.user-popover').hide();
+        }
+    });
+
+    $('.user').on('click', function () {
+        $('.user').toggleClass('popover-open');
+        $('.user-popover').toggle();
+    });
+});
